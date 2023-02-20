4f6945360aac91c47523a820b3aa7d09b2e7162d
Elena Dragun <edragun09@gmail.com>
2023-02-21 00:12:42 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+
+const addMarkup = galleryItems.map(pic => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${pic.original}">
+    <img
+      class="gallery__image"
+      src="${pic.preview}"
+      data-source="${pic.original}"
+      alt="${pic.description}"
+    />
+  </a>
+</div>`
+}).join('');
+
+
+gallery.insertAdjacentHTML('beforeend', addMarkup);
+gallery.addEventListener('click', clickOnOpen);
+
+function clickOnOpen(event) {};
+
+
