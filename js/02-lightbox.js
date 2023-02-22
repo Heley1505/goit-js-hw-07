c901d7d2ed4c43399c74dffd1e5315f3d2aa3d40
Elena Dragun <edragun09@gmail.com>
2023-02-23 00:51:46 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const addMarkup = galleryItems.map(pic => {
+    return `<a class="gallery__item" href="${pic.original}">
+  <img class="gallery__image" src="${pic.preview}" alt="${pic.description}" />
+</a>`}).join('');
+
+gallery.insertAdjacentHTML('afterbegin', addMarkup);
+
+const simpleLightbox = new SimpleLightbox(`.gallery a`, {
+    captionsData: "alt",
+    captionsDelay: 250,
+});
